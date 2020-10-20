954225bcc4e6f78bf624c012ec5c7c735cadc11f
Kushal Jaiswal <68225773+kudy01@users.noreply.github.com>
2020-10-20 14:32:24 +0530

    Updates

---
+++ precache-manifest.b9b378b8fd6d443c7d96cdd4a9702b3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ea5122ad9235eb61fb9b5f5c426e1f4",
+    "url": "/Quote-Generator/index.html"
+  },
+  {
+    "revision": "0d19f08036d5ec6dbaa4",
+    "url": "/Quote-Generator/static/css/main.9010c107.chunk.css"
+  },
+  {
+    "revision": "f6cd76965ae839ec651b",
+    "url": "/Quote-Generator/static/js/2.2daf27ad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Quote-Generator/static/js/2.2daf27ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d19f08036d5ec6dbaa4",
+    "url": "/Quote-Generator/static/js/main.cf407395.chunk.js"
+  },
+  {
+    "revision": "68cea268cbb09478a60f",
+    "url": "/Quote-Generator/static/js/runtime-main.8f36aec5.js"
+  }
+]);
